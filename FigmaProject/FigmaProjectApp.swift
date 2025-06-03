e2e48c9dc7bb11f4ec6aadff87d109dee4ed2565
Wlad Madi <wlad@madi.space>
2025-06-03 16:03:15 +0300

    Initial Commit

---
+++ FigmaProject/FigmaProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FigmaProjectApp.swift
+//  FigmaProject
+//
+//  Created by Влад Мади on 03.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct FigmaProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
